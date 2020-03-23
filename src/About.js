d142192b9778f5d99c964bfc1a5764058969b5e7
Dušan Simić <dusan.simic1810@gmail.com>
2020-03-23 19:53:29 +0100

    Type fixes and added important notice

--- src/About.js
+++ src/About.js
@@ -15,18 +15,24 @@ export default function About() {
 						</Navbar>
 						<h3><b>Idea</b></h3>
 						<p>
-							This is a small, "quick and dirty", simple, web app that I made. My mom who is a medical specialist wanted to see graphs with numbers of Corona virus patients in each country for the past cuple of weeks. There are some sites where you can get that but not for all countries as I've seen. Since it was good for my mom and me I wanted to share it so anyone can check it out.
+							This is a small, "quick and dirty", simple, web app that I made. I wanted to see graphs with numbers of Corona virus patients in each country for the past couple of weeks. There are some sites where you can get that but not for all countries as I've seen. Since it was good for me I wanted to share it so anyone can check it out.
 						</p>
 
 						<h3><b>Data source</b></h3>
 						<p>
 							All of the data You see here is from <a href="https://github.com/CSSEGISandData/COVID-19">this</a> GitHub repository. It's created by Center for Systems Science and Engineering at the Johns Hopkins University. You can see all the information about the data sources on that link.<br/>
-							The data is now always up to date because it's updated once at midnight (23:59h) but it's reliable and could be used for relative representation of the Corona virus situation around the world.
+							The data is not always up to date because it's updated once at midnight (23:59h) but it's reliable and could be used for relative representation of the Corona virus situation around the world.
 						</p>
 
 						<h3><b>Open source</b></h3>
 						<p>
 							This web app is open source under the MIT license. You can find it on my GitHub <a href="https://github.com/dusansimic/covid19-graphs">repo</a>. I'm open to suggestions and change requests. You can create an Issue on the <a href="https://github.com/dusansimic/covid19-graphs/issues?q=is%3Aissue+is%3Aopen+sort%3Aupdated-desc">issue page</a> or if you don't have a GitHub account, just <a href="mailto:dusan.simic1810@gmail.com">email me</a>.
+						</p>
+
+						<h3><b>Important note</b></h3>
+						<p>
+							As it is stated in terms of use in CSSE GitHub repository:<br/>
+							<i>Reliance on the Website for medical guidance or use of the Website in commerce is strictly prohibited</i><br/>
 						</p>
 						<p style={{margin: '30px 0'}}><center>MIT © <a href="http://dusansimic.me">Dušan Simić</a></center></p>
 					</Col>
