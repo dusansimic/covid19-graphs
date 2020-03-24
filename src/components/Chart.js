65a890853269583f7741f29c7c4c46495ae4f898
Dušan Simić <dusan.simic1810@gmail.com>
2020-03-24 12:10:02 +0100

    Hardcode options into chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -2,7 +2,18 @@ import React from 'react';
 import {Line} from 'react-chartjs-2';
 
 export default function Chart(props) {
+	const options = {
+		tooltips: {
+			mode: 'index',
+			intersect: false
+		},
+		hover	: {
+			mode: 'index',
+			intersect: true
+		}
+	};
+
 	return (
-			<Line data={props.data} />
+			<Line data={props.data} options={options}/>
 	);
 };
